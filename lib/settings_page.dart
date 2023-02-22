1491d387626d91a9fc420219ddc1da5cb2fb78ba
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2023-02-22 17:26:43 +0100

    settings wakeup time

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -28,9 +28,29 @@ class SettingsPage extends StatelessWidget {
     return bb;
   }
 
+  Widget buildWakeUpTime(BuildContext context) {
+    final localizations = MaterialLocalizations.of(context);
+    final Clock clock = Provider.of<Clock>(context, listen: true);
+    TimeOfDay wakeUpTime = clock.wakeUpTime;
+    final formattedWakeUpTime =
+        localizations.formatTimeOfDay(wakeUpTime, alwaysUse24HourFormat: true);
+
+    final wakeUpTimeButton = TextButton(
+        onPressed: () async {
+          TimeOfDay? wkt = await showTimePicker(
+              context: context,
+              initialTime:
+                  TimeOfDay(hour: wakeUpTime.hour, minute: wakeUpTime.minute));
+          clock.wakeUpTime = wkt ?? wakeUpTime;
+        },
+        child: Text(formattedWakeUpTime));
+    return wakeUpTimeButton;
+  }
+
   Widget buildContent(BuildContext context) {
     List<Widget> childrenList = [
       buildCurrentTime(context),
+      buildWakeUpTime(context),
       buildBackButton(context)
     ];
     return Padding(
